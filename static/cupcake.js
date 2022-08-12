b97a460fcb7c8e3828e84a105874757f0ee51e2c
Patrick Sontag <pmsontag84@gmail.com>
2022-08-12 11:40:09 -0500

    jquery delete button

--- static/cupcake.js
+++ static/cupcake.js
@@ -1,1 +1,7 @@
-alert("hello?")
+$('.delete-btn').click(deleteCupcake)
+    
+async function deleteCupcake() {
+    const id = $(this).data('id')
+    await axios.delete(`api/cupcakes/${id}`)
+    $(this).parent().remove()
+}
